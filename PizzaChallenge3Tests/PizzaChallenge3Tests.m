b6731827db67798fcfe77ee16d6bb66bdd5b179f
May Yang <mayyang@May-Yangs-MacBook-Pro.local>
2014-11-05 19:15:25 -0800

    Initial Commit

---
+++ PizzaChallenge3Tests/PizzaChallenge3Tests.m
@@ -0,0 +1,40 @@
+//
+//  PizzaChallenge3Tests.m
+//  PizzaChallenge3Tests
+//
+//  Created by May Yang on 11/5/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PizzaChallenge3Tests : XCTestCase
+
+@end
+
+@implementation PizzaChallenge3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
